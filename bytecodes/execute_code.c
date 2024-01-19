a1e481044a7aeef178aa587a11a941175c3e6f2b
towera7973 <@towera7973@gmail.com@>
2024-01-18 17:05:22 -0800

    changed directories

---
+++ bytecodes/execute_code.c
@@ -0,0 +1,65 @@
+#include "monty.h"
+
+/**
+ * execute_file - reads and executes opcodes from a file
+ * @stack: pointer to the top of the stack
+ */
+void execute_file(stack_t **stack)
+{
+	char *opcode = NULL;
+	size_t len = 0;
+	ssize_t nread;
+	unsigned int line_number = 0;
+
+	while ((nread = getline(&glob.line, &len, glob.file)) != -1)
+	{
+		line_number++;
+		opcode = strtok(glob.line, " \t\r\n\a");
+		glob.arg = strtok(NULL, " \n\t");
+		if (opcode == NULL || *opcode == '#')
+			continue;
+		execute_opcode(opcode, stack, line_number);
+	}
+}
+
+/**
+ * execute_opcode - executes a single opcode
+ * @opcode: opcode to execute
+ * @stack: pointer to the top of the stack
+ * @line_number: line number of the opcode
+ */
+void execute_opcode(char *opcode, stack_t **stack, unsigned int line_number)
+{
+	instruction_t instructions[] = {
+		{"push", _push},
+		{"pall", _pall},
+		{"pint", _pint},
+		{"pop", _pop},
+		{"swap", _swap},
+		{"nop", _nop},
+		{"add", _add},
+		{"sub", _sub},
+		{"div", _div},
+		{"mul", _mul},
+		{"mod", _mod},
+		{"pchar", _pchar},
+		{"pstr", _pstr},
+		{NULL, NULL}
+	};
+	int i;
+
+	for (i = 0; instructions[i].opcode != NULL; i++)
+	{
+		if (strcmp(opcode, instructions[i].opcode) == 0)
+		{
+			instructions[i].f(stack, line_number);
+			return;
+		}
+	}
+
+	fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
+	free_stack(*stack);
+	fclose(glob.file);
+	free(glob.line);
+	exit(EXIT_FAILURE);
+}
